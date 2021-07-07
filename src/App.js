21a69a65a6c6a850385e01544285cb27c5617286
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-07-07 00:30:49 -0500

    added some reactstrap funtionality to my page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import './App.css';
-import React from 'react'
-import {Link, Route} from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, Route } from 'react-router-dom'
 import Projects from './components/Projects'
 import About from './components/About'
 import Contact from './components/Contact'
-
-
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem} from 'reactstrap'
 
 
 function App() {
+  const [collapsed, setCollapsed] = useState(true)
+
+  const clickHandler = () => {
+    setCollapsed(!collapsed)
+  }
+
   return (
-    <div className="App">
-      <header className='App-header'>
-        <div className="logo">
-          <Link to ="/">Home</Link>
-        </div>
-        <Link to="/projects">Projects</Link>
-        <Link to="/about">About</Link>
-        <Link to ="/contact">Contact</Link>
+    <div> 
+      <Navbar color="faded" light>
+        <NavbarBrand className="mr-auto">
+          <Link to="/">Home</Link>
+        </NavbarBrand>
+        <NavbarToggler onClick={clickHandler} size="lg" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar size="lg">
+            <NavItem>
+              <Link to="/projects">Projects</Link>
+            </NavItem>
+            
+            <NavItem>
+              <Link to="/about">About</Link>
+            </NavItem>
+            <NavItem>
+              <Link to ="/contact">Contact</Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
         
-      </header>
         <Route exact path="/" />
         <Route path="/projects" component={Projects} />
         <Route path="/about" component={About} />
